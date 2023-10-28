1c0a0d3c38e6121e73ae4a1a47630b435f2b4a99
private_diary <https://github.com/Fitst-droid/private_diary.git>
2023-10-28 10:57:06 +0900

    スーパーユーザー作成

--- private_diary/settings_common.py
+++ private_diary/settings_common.py
@@ -206,12 +206,11 @@ ACCOUNT_USERNAME_REQUIRED = False
 
 # サインアップにメールアドレス確認をはさむよう設定
 ACCOUNT_EMAIL_VERIFICATION = 'mandatory'
-ACCOUNT_EMAIL_REQUIRED = 'account_login'
+ACCOUNT_EMAIL_REQUIRED = True
 
 # ログイン/ログアウト後の遷移先を設定
-LOGIN_REDIRECT_URL = 'diary.index_list'
+LOGIN_REDIRECT_URL = 'diary:diary_list'
 ACCOUNT_LOGOUT_REDIRECT_URL = 'account_login'
-
 
 # ログアウトリンクのクリック一発でログアウトする設定
 ACCOUNT_LOGOUT_ON_GET = True
